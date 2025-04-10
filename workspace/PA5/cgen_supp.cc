239b1c3af776ad3a0b98d4b59cdb9af09786ea86
Shun Usami <usatie@gmail.com>
2025-04-10 14:19:01 -0700

    Format initial source code with clang-format

--- workspace/PA5/cgen_supp.cc
+++ workspace/PA5/cgen_supp.cc
@@ -1,30 +1,25 @@
+#include "stringtab.h"
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
-#include "stringtab.h"
 
 static int ascii = 0;
 
-void ascii_mode(ostream& str)
-{
-  if (!ascii) 
-    {
-      str << "\t.ascii\t\"";
-      ascii = 1;
-    } 
+void ascii_mode(ostream &str) {
+  if (!ascii) {
+    str << "\t.ascii\t\"";
+    ascii = 1;
+  }
 }
 
-void byte_mode(ostream& str)
-{
-  if (ascii) 
-    {
-      str << "\"\n";
-      ascii = 0;
-    }
+void byte_mode(ostream &str) {
+  if (ascii) {
+    str << "\"\n";
+    ascii = 0;
+  }
 }
 
-void emit_string_constant(ostream& str, char* s)
-{
+void emit_string_constant(ostream &str, char *s) {
   ascii = 0;
 
   while (*s) {
@@ -39,23 +34,20 @@ void emit_string_constant(ostream& str, char* s)
       break;
     case '\\':
       byte_mode(str);
-      str << "\t.byte\t" << (int) ((unsigned char) '\\') << endl;
+      str << "\t.byte\t" << (int)((unsigned char)'\\') << endl;
       break;
-    case '"' :
+    case '"':
       ascii_mode(str);
       str << "\\\"";
       break;
     default:
-      if (*s >= ' ' && ((unsigned char) *s) < 128) 
-	{
-	  ascii_mode(str);
-	  str << *s;
-	}
-      else 
-	{
-	  byte_mode(str);
-	  str << "\t.byte\t" << (int) ((unsigned char) *s) << endl;
-	}
+      if (*s >= ' ' && ((unsigned char)*s) < 128) {
+        ascii_mode(str);
+        str << *s;
+      } else {
+        byte_mode(str);
+        str << "\t.byte\t" << (int)((unsigned char)*s) << endl;
+      }
       break;
     }
     s++;
@@ -63,5 +55,3 @@ void emit_string_constant(ostream& str, char* s)
   byte_mode(str);
   str << "\t.byte\t0\t" << endl;
 }
-
-
